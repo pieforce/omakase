78f44a98b7391707eba7de82ae38080591080541
Wesley <wing3000@gmail.com>
2018-06-16 02:44:41 -0700

    Added voting route. Brain now learns data from mongodb.

--- app/app.js
+++ app/app.js
@@ -3,20 +3,30 @@
 const yelp = require('yelp-fusion');
 const brain = require('brain.js');
 const fs = require('fs');
+const mongo = require('mongodb').MongoClient;
 const apiConfig = require('../config/api');
 
 // Constants
 const TRAINING_DATA_PATH = './config/brainTrainingData.json';
 const YELP_CLIENT = yelp.client(apiConfig.yelpApiKey);
-const MAX_NUM_REVIEWS = 10000;
+const MAX_NUM_REVIEWS = 100000;
 const MAX_NUM_STARS = 5;
 const MAX_NUM_DOLLAR_SIGNS = 4;
 
 // Train neural net
-var trainingDataJson = JSON.parse(fs.readFileSync(TRAINING_DATA_PATH, 'utf8'));
 var net = new brain.NeuralNetwork();
-net.train(trainingDataJson);
-let trainedNet = net.toFunction();
+var trainedNet = null;
+mongo.connect(apiConfig.mlabUrl, (err, db) => {
+    if (err) throw err;
+    var dbo = db.db(apiConfig.mlabDatabase);
+    dbo.collection(apiConfig.mlabCollection).findOne({'info.snapshot' : apiConfig.mlabSnapshot}, function(err, result) {
+        if (err) throw err;
+        console.log(result.data);
+        net.train(result.data);
+        trainedNet = net.toFunction();
+        db.close();
+    });
+});
 
 // API Routes
 module.exports = function (app) {
@@ -34,7 +44,6 @@ module.exports = function (app) {
         } else if (resultLimit < 0) {
             resultLimit = 5;
         }
-        console.log('resultLimit: ' + resultLimit)
 
         YELP_CLIENT.search({
             term: searchStr,
@@ -48,11 +57,15 @@ module.exports = function (app) {
                 // If no price is returned, assume one dollar sign ($ / $$$$) or 1/4 = 0.25
                 // If valid price range, count # of dollar signs
                 var resultPrice = isEmpty(result.price) ? 0.25 : ((result.price.split('$').length - 1) / MAX_NUM_DOLLAR_SIGNS);
-                var resultReviewCount = isEmpty(result.rating) ? 0 : result.review_count / MAX_NUM_REVIEWS;
+                var resultReviewCount = isEmpty(result.review_count) ? 0 : result.review_count / MAX_NUM_REVIEWS;
                 var resultRating = isEmpty(result.rating) ? 0 : result.rating / MAX_NUM_STARS;
+                var resultId = getHash(result.id);
+
+                console.log(resultId);
     
                 // Run neural net analysis and print result
-                var output = trainedNet({ 
+                var output = net.run({ 
+                    id : resultId,
                     review_count: resultReviewCount,
                     rating: resultRating,
                     price: resultPrice
@@ -75,6 +88,73 @@ module.exports = function (app) {
         });
     });
 
+    // Get appeal of a business based on Yelp ID
+    app.get('/api/appeal/:id', function (req, res) {
+        YELP_CLIENT.business(req.params.id).then(response => {
+            var result = response.jsonBody;
+            // If no price is returned, assume one dollar sign ($ / $$$$) or 1/4 = 0.25
+            // If valid price range, count # of dollar signs
+            var resultPrice = isEmpty(result.price) ? 0.25 : ((result.price.split('$').length - 1) / MAX_NUM_DOLLAR_SIGNS);
+            var resultReviewCount = isEmpty(result.review_count) ? 0 : result.review_count / MAX_NUM_REVIEWS;
+            var resultRating = isEmpty(result.rating) ? 0 : result.rating / MAX_NUM_STARS;
+            var resultRestId = getHash(req.params.id);
+
+            // Run neural net analysis and print result
+            var output = net.run({ 
+                id: resultRestId,
+                review_count: resultReviewCount,
+                rating: resultRating,
+                price: resultPrice
+            });
+            // Return appeal as percentage
+            res.json({'id': resultRestId, 'appeal': (output.appeal * 100).toFixed(0)});
+        }).catch(e => {
+            console.log(e);
+        });
+    });
+
+    // Train brain by voting for a business
+    app.get('/api/vote/:id/:reviewCount/:numStars/:numDollarSigns/:appeal/:rating', function (req, res) {
+        // If no price is returned, assume one dollar sign ($ / $$$$) or 1/4 = 0.25
+        // If valid price range, count # of dollar signs
+        var restId = getHash(req.params.id);
+        var reviewCount = req.params.reviewCount;
+        var numDollarSigns = req.params.numDollarSigns;
+        var numStars = req.params.numStars;
+        var appeal = req.params.appeal;
+        var userRating = req.params.rating;
+
+        numDollarSigns = isEmpty(numDollarSigns) ? 0.25 : (numDollarSigns / MAX_NUM_DOLLAR_SIGNS);
+        reviewCount = isEmpty(reviewCount) ? 0 : reviewCount / MAX_NUM_REVIEWS;
+        numStars = isEmpty(numStars) ? 0 : numStars / MAX_NUM_STARS;
+        appeal = isEmpty(appeal) ? 0 : appeal / 100;
+        userRating = (isEmpty(userRating) ? 0 : userRating / MAX_NUM_STARS);
+
+        // Formula for adjusting current appeal based on user input
+        //   *Assumption: userRating is a float between 0 and 1
+        //   *Assumption: appeal is a float between 0 and 1
+        var adjAppeal = appeal * (1 + ((userRating - appeal) * 0.1))
+
+        console.log('restId ' + restId);
+        console.log('appeal ' + appeal);
+        console.log('adjAppeal ' + adjAppeal);
+        console.log('reviewCount ' + reviewCount);
+        console.log('Rated ' + req.params.id + ': ' + userRating);
+        console.log('New user appeal ' + req.params.id + ': ' + userRating);
+        net.train({ 
+            input: {
+                id: restId,
+                review_count: reviewCount,
+                rating: numStars,
+                price: numDollarSigns
+            },
+            output: {
+                appeal: adjAppeal
+            }
+        });
+        res.json({'id': restId, 'appeal': (adjAppeal * 100).toFixed(0)});
+    });
+
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
         res.sendFile(__dirname + '/index.html'); // load the single view file (angular will handle the page changes on the front-end)
@@ -87,4 +167,18 @@ function isEmpty(str) {
     } else {
         false;
     }
+}
+
+function getHash(str) {
+    var hash = 0;
+    if (str.length == 0) return hash;
+    for (var i = 0; i < str.length; i++) {
+        var char = str.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer08
+    }
+    hash = '0.' + Math.abs(hash);
+    console.log(hash);
+    return hash;
+
 }
--- config/api.js
+++ config/api.js
@@ -1,3 +1,8 @@
 module.exports = {
-    yelpApiKey : 'GQy2ALXHhP-dQgRYZUWZ8yVn_IUgc2U2Tg_Ah4ONThDYwtLG3gh_8s1fEQR5GYukmFswGkRgf1BImmWgsHXes48S7H927fN5kooT_Zt-ursn4LZX-uKqW83C6qEaW3Yx'
+    yelpApiKey : 'GQy2ALXHhP-dQgRYZUWZ8yVn_IUgc2U2Tg_Ah4ONThDYwtLG3gh_8s1fEQR5GYukmFswGkRgf1BImmWgsHXes48S7H927fN5kooT_Zt-ursn4LZX-uKqW83C6qEaW3Yx',
+    mlabUrl : 'mongodb://omakasereadwrite:OmakaseReadWrite1@ds163410.mlab.com:63410/omakase',
+    mlabDatabase : 'omakase',
+    mlabCollection : 'brain',
+    // mlabSnapshot : 'Sandbox'
+    mlabSnapshot : '06-15-2018-23:44'
 };
