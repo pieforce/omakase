10049468b6859a71d16c960e87687db6674f1eac
Wesley <wing3000@gmail.com>
2018-06-17 02:54:53 -0700

    Added prompt to get location data, changes to look and feel of UI

--- public/core.js
+++ public/core.js
@@ -1,13 +1,38 @@
 var omakase = angular.module('omakase', []);
 
-function mainController($scope, $http) {
+function mainController($scope, $http) {    
+    const NUM_OF_STARS = 5;  
+    const NUM_OF_RESULTS = 30;
     $scope.searchStr = '';
     $scope.location = '';
+    $scope.accuracy = '';
     $scope.loading = false;
+    $scope.showLocationEntry = false;
+    $scope.showAccuracy = false;
+
+    $scope.toggleLocationEntry = function() {
+        $scope.showLocationEntry = !$scope.showLocationEntry;
+    }
+
+    $scope.getLocation = function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(pos) {
+                var longitude = pos.coords.longitude;
+                var latitude = pos.coords.latitude;
+                var accuracy = pos.coords.accuracy;
+                $scope.showAccuracy = true;
+                $scope.location.text = longitude + ', ' + latitude;
+                document.getElementById('accuracy').textContent = 'GPS accuracy: ' + (accuracy * 0.00062137).toFixed(2) + ' mi';
+                document.getElementById('location').value = longitude + ', ' + latitude;
+            });
+        } else {
+            alert('Geolocation is not supported by this browser');
+        }
+    }
 
     $scope.getResults = function(searchStr) {
         $scope.loading = true;
-        $http.get('/api/search/' + searchStr + '/' +  $scope.location.text + '/30')
+        $http.get('/api/search/' + searchStr + '/' +  $scope.location.text + '/' + NUM_OF_RESULTS)
             .success(function(data) {
                 $scope.results = data;
                 $scope.loading = false;
@@ -33,22 +58,7 @@ function mainController($scope, $http) {
 
     $scope.getRestaurantResults = function() {
         $scope.loading = true;
-        $http.get('/api/search/restaurants/' +  $scope.location.text + '/30')
-            .success(function(data) {
-                $scope.results = data;
-                $scope.loading = false;
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-                $scope.loading = false;
-            })
-    }
-
-    
-
-    $scope.getTopRestaurantResult = function() {
-        $scope.loading = true;
-        $http.get('/api/search/restaurants/' +  $scope.location.text + '/1')
+        $http.get('/api/search/restaurants/' +  $scope.location.text + '/' + NUM_OF_RESULTS)
             .success(function(data) {
                 $scope.results = data;
                 $scope.loading = false;
@@ -69,20 +79,35 @@ function mainController($scope, $http) {
             })
     }
 
-    $scope.rate = function(id, reviewCount, numStars, numDollarSigns, appeal, rating) {
+    $scope.updateUserRating = function(id, rating) {
         // Update stars to reflect user rating
-        for (var i=1; i <= rating; i++) {
-            document.getElementsByName(id + '-' + i)[0].setAttribute('class', 'btn btn-danger btn-sm');
+        for (var i=1; i <= NUM_OF_STARS; i++) {
+            if (i <= rating) {
+                document.getElementsByName(id + '-' + i)[0].setAttribute('class', 'btn btn-danger btn-sm');
+            } else {
+                document.getElementsByName(id + '-' + i)[0].setAttribute('class', 'btn btn-outline-danger btn-sm');
+            }
         }
+    }
+
+    $scope.submitRating = function(id, reviewCount, numStars, numDollarSigns, appeal) {
+        // Get number of stars
+        var rating = document.querySelectorAll('.rating-form-' + id + ' > [class="btn btn-danger btn-sm"]').length;
+        if (rating <= 0 || rating > 5) {
+            return;
+        }
+
+        // Hide submit link
+        document.getElementsByName('rating-submit-' + id)[0].setAttribute('style', 'display:none');
+
         // Disable rating star buttons
-        const NUM_OF_STARS = 5;
         for (var i=1; i <= NUM_OF_STARS; i++) {
             document.getElementsByName(id + '-' + i)[0].setAttribute('disabled', 'true');
         }
-        
 
+        // Submit rating
         numDollarSigns = typeof(numDollarSigns) == "undefined" ? 1 : numDollarSigns.length;
-        $http.get('/api/vote/' + id + '/' + reviewCount + '/' + numStars + '/' + numDollarSigns + '/' + appeal + '/' + rating)
+        $http.get('/api/rate/' + id + '/' + reviewCount + '/' + numStars + '/' + numDollarSigns + '/' + appeal + '/' + rating)
             .success(function(data) {
                 console.log('Rated: ' + data.appeal);
             })
