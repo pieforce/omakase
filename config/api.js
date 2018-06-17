0523fde821edd4d87cd61a530cc858c5ed0c4d17
Wesley <wing3000@gmail.com>
2018-06-17 02:53:33 -0700

    Adjusted learning data to 100k max # reviews, more forgiving of lesser # of reviews

--- config/api.js
+++ config/api.js
@@ -3,5 +3,5 @@ module.exports = {
     mlabUrl : 'mongodb://omakasereadwrite:OmakaseReadWrite1@ds163410.mlab.com:63410/omakase',
     mlabDatabase : 'omakase',
     mlabCollection : 'brain',
-    mlabSnapshot : '06-16-2018-18:12'
+    mlabSnapshot : '06-17-2018-00:38'
 };
